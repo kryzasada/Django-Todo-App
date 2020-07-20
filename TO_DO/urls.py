e47aa791a20a488dffe25e3c9751b08dffa57d76
krystian <krystian.zasada@op.pl>
2020-07-21 00:59:27 +0200

    Fix template_name in default page

--- TO_DO/urls.py
+++ TO_DO/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('login', auth_views.LoginView.as_view(template_name='login.html'),  name='login'),
     path('logout', auth_views.LogoutView.as_view(template_name='logout.html'),  name='logout'),
     path('login1', user_login_in,  name='zalogowany'),
-    path('', auth_views.LoginView.as_view(template_name='register.html'),  name='start'),
+    path('', auth_views.LoginView.as_view(template_name='login.html'),  name='start'),
 ]
