d216f4b0d037b0b3a7e116638fd64ee78e80ca52
krystian <krystian.zasada@op.pl>
2020-07-28 17:59:00 +0200

    Rename UserPosts -> UserPost

--- posts/admin.py
+++ posts/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import UserPosts
+from .models import UserPost
 
-admin.site.register(UserPosts)
+admin.site.register(UserPost)
--- posts/models.py
+++ posts/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 
 user_model = get_user_model()
 
-class UserPosts(models.Model):
+class UserPost(models.Model):
     date = models.DateField()
     text = models.TextField()
     users = models.ForeignKey(user_model, on_delete=models.CASCADE)
