495594707a2915881584eea10cfb034a2763e642
krystian <krystian.zasada@op.pl>
2020-07-28 17:54:29 +0200

    Create model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+user_model = get_user_model()
+
+class UserPosts(models.Model):
+    date = models.DateField()
+    text = models.TextField()
+    users = models.ForeignKey(user_model, on_delete=models.CASCADE)
