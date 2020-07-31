143a68c2b4468188d1a220cf2b700e3b3c360580
krystian <krystian.zasada@op.pl>
2020-07-31 16:46:52 +0200

    Transfer user TODO list

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .forms import UserPostForms
+from .models import UserPost
+from django.db.models import Q
+
 
 # Create your views here.
 
@@ -11,13 +14,18 @@ def user_login_in(request, *args, **kwargs):
         form = UserPostForms(request.POST)
 
         if form.is_valid():
-             post = form.save(commit=True)
+            form.save(commit=True)
+            form = UserPostForms()
 
     else:
         form = UserPostForms()
 
+    obj = UserPost.objects.filter(users=current_user.id)
+
     context = {
         'form': form,
         'user_id': current_user.id,
+        #'obj': obj.values('text'),
+        'obj': obj,
     }
     return render(request, 'logged.html', context)
